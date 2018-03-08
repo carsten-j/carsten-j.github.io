f2e4d80317a39072db3ddd2412080d00043913cb
carstenj <carstenj@gmail.com>
2018-03-08 19:08:40 +0100

    Minor updates

--- jekyll.sh
+++ jekyll.sh
@@ -1,1 +1,5 @@
-docker run --rm --label=jekyll --volume /Users/carsten/Projects/carsten-j.github.io:/srv/jekyll -it -p 4000:4000 jekyll/jekyll jekyll serve  --drafts
+export JEKYLL_VERSION=3.5
+docker run --rm \
+  --volume="$PWD:/srv/jekyll" \
+  -it -p 4000:4000 jekyll/minimal:$JEKYLL_VERSION \
+  jekyll serve
